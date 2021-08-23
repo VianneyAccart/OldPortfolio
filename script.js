7cf513838b98e23485d901669f970791c0cc6953
Vianney Accart <vianneyaccart@gmail.com>
2021-08-23 18:52:25 +0200

    Ajout localStorage pour conserver le mode choisi

--- script.js
+++ script.js
@@ -4,9 +4,20 @@ const theme = document.querySelector("#theme-link");
 btn.addEventListener("click", function() {
   if (theme.getAttribute("href") == "light-theme.css") { // Si #theme-link contient "light-theme.css"...
     theme.href = "dark-theme.css"; // Modifier en "dark-theme.css"
+    localStorage.setItem("theme", "dark");
     document.getElementById("link-toggle").className = "bi bi-brightness-high"; // Et changer l'icône
   } else {
     theme.href = "light-theme.css";
+    localStorage.setItem("theme", "light");
     document.getElementById("link-toggle").className = "bi bi-moon";
   }
 });
+
+if(localStorage.getItem("theme") == "dark"){
+  theme.href = "dark-theme.css";
+  document.getElementById("link-toggle").className = "bi bi-brightness-high";
+} else {
+  theme.href = "light-theme.css";
+  document.getElementById("link-toggle").className = "bi bi-moon";
+};
+
